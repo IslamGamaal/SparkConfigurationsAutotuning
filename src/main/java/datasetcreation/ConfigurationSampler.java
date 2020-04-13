a345e9cb0f7fe53e5571098c3661a367aaaf277b
akrammousa <akrammousa2014@gmail.com>
2020-04-13 03:40:51 +0200

    [master] Configurations sampler interface functions.

--- src/main/java/datasetcreation/ConfigurationSampler.java
+++ src/main/java/datasetcreation/ConfigurationSampler.java
@@ -1,4 +1,25 @@
 package datasetcreation;
 
+import java.util.List;
+
 public interface ConfigurationSampler {
+
+    /**
+     * This function generate the configurations samples values.
+     * @param samplesNum int: containing samples number needed to be generated.
+     * @param upperBounds List : containing the upper bounds of the configurations.
+     * @param lowerBounds List : containing the lower bounds of the configurations.
+     * @return List<List<Float>>: containing lists of configurations samples.
+     **/
+
+    List<List<Float>> generateSamples(int samplesNum , List<Float> upperBounds , List<Float> lowerBounds);
+
+
+    /**
+     * This functions writes the configurations samples in files .conf
+     *
+     * @param configurationsFolderPath String : containing path of the folder where the configurations will be written.
+     * @param samples List<List<Float>> : containing lists of configurations samples.
+     **/
+    void writeConfigurationsSamplesInFiles(String configurationsFolderPath , List<List<Float>> samples);
 }
