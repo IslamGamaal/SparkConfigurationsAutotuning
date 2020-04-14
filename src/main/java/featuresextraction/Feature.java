2130155a1649ec1c83ecf302844912c4843b9447
IslamGamaal <islamgamal_1997@hotmail.com>
2020-04-14 16:12:42 +0200

    Adding feature class.

--- src/main/java/featuresextraction/Feature.java
+++ src/main/java/featuresextraction/Feature.java
@@ -1,5 +1,19 @@
 package featuresextraction;
 
 public class Feature {
+    private String featureName;
+    private double value;
 
+    public Feature(String featureName, double value) {
+        this.featureName = featureName;
+        this.value = value;
+    }
+
+    public String getFeatureName() {
+        return this.featureName;
+    }
+
+    public double getValue() {
+        return this.value;
+    }
 }
