0bb606bdeabcbf85066f1910ba5c94a38b51d1c5
IslamGamaal <islamgamal_1997@hotmail.com>
2020-04-14 16:56:45 +0200

    Initialization Handler Skeleton.

--- src/main/java/InitializationHandler.java
+++ src/main/java/InitializationHandler.java
@@ -1,2 +1,3 @@
 public interface InitializationHandler {
+    boolean instantiationTrigger(String pathToJars);
 }
---
+++ src/main/java/InitializationHandlerImpl.java
@@ -0,0 +1,5 @@
+public class InitializationHandlerImpl implements InitializationHandler {
+    public boolean instantiationTrigger(String pathToJars) {
+        return true;
+    }
+}
