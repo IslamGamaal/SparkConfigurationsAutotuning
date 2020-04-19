05637d95c0ce5d9363832dce789b48720719d642
IslamGamaal <islamgamal_1997@hotmail.com>
2020-04-19 22:57:24 +0200

    Adding fields to SparkApplication class

--- src/main/java/spark/utilites/SparkApplication.java
+++ src/main/java/spark/utilites/SparkApplication.java
@@ -1,4 +1,19 @@
 package spark.utilites;
 
 public class SparkApplication {
+    private String optimizedQueryPlan = "";
+    private String physicalPlan = "";
+    private String stagesJson = "";
+
+    public String getOptimizedQueryPlan() {
+        return optimizedQueryPlan;
+    }
+
+    public String getPhysicalPlan() {
+        return physicalPlan;
+    }
+
+    public String getStagesJson() {
+        return stagesJson;
+    }
 }
