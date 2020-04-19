78c4af5c248fb3696d17ebffdd841f4a50fc345d
IslamGamaal <islamgamal_1997@hotmail.com>
2020-04-19 23:16:46 +0200

    Mocking the FeatureExtractionHandler class.

--- src/test/java/featuresextraction/FeaturesExtractionHandlerTest.java
+++ src/test/java/featuresextraction/FeaturesExtractionHandlerTest.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
+import spark.SparkHandler;
 import spark.utilites.SparkApplication;
 
 import java.util.ArrayList;
@@ -21,22 +22,40 @@ public class FeaturesExtractionHandlerTest {
     FeaturesExtractionHandler featuresExtractionHandler;
     StaticFeaturesExtractor staticFeaturesExtractor;
     DynamicFeaturesExtractor dynamicFeaturesExtractor;
+    SparkHandler sparkHandler;
 
 
     @Before
     public void before() {
         featuresExtractionHandler = mock(FeaturesExtractionHandlerImp.class);
-        staticFeaturesExtractor = mock(StaticFeaturesExtractor.class);
-        dynamicFeaturesExtractor = mock(DynamicFeaturesExtractor.class);
+        staticFeaturesExtractor = mock(StaticFeaturesExtractorImp.class);
+        dynamicFeaturesExtractor = mock(DynamicFeaturesExtractorImp.class);
+        sparkHandler = mock(SparkHandler.class);
 
         when(staticFeaturesExtractor.extract(anyString(), anyString())).thenReturn(new ArrayList<Feature>());
         when(dynamicFeaturesExtractor.extract(anyString())).thenReturn(new ArrayList<Feature>());
-
+        when(sparkHandler.HandleApplication(any(SparkApplication.class))).thenReturn(new SparkApplication());
+        List<Feature> extractedFeatures = simulateExtractFeatures(new SparkApplication());
+        when(featuresExtractionHandler.extract(any(SparkApplication.class))).thenReturn(extractedFeatures);
     }
 
 
     @Test
-    public void PredictSuitableConfigurationsTest() {
+    public void extractAllFeaturesTest() {
+        List<Feature> actual = featuresExtractionHandler.extract(new SparkApplication());
+        Assert.assertEquals(new ArrayList<Feature>(), actual);
+    }
 
+    private List<Feature> simulateExtractFeatures(SparkApplication sparkApplication) {
+        SparkApplication outputApplication = sparkHandler.HandleApplication(sparkApplication);
+        String optimizedQueryPlan = outputApplication.getOptimizedQueryPlan();
+        String physicalPlan = outputApplication.getPhysicalPlan();
+        String stagesJson = outputApplication.getStagesJson();
+        List<Feature> staticFeatures = staticFeaturesExtractor.extract(physicalPlan, optimizedQueryPlan);
+        List<Feature> dynamicFeatures = dynamicFeaturesExtractor.extract(stagesJson);
+        List<Feature> features = new ArrayList<Feature>();
+        features.addAll(staticFeatures);
+        features.addAll(dynamicFeatures);
+        return features;
     }
 }
