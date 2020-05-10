d5cb271568ca77c49ba45167b1485b196db9495e
Sarah.eldafrawy <sarah.eldafrawy257@gmail.com>
2020-05-11 00:23:34 +0200

    BLHS

--- src/main/java/configurations/utilites/utils.java
+++ src/main/java/configurations/utilites/utils.java
@@ -1,6 +1,5 @@
 package configurations.utilites;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class utils {
@@ -14,30 +13,31 @@ public class utils {
      * @param newValues
      * @return
      */
-    public static List<Configuration> getConfigurationsList( List<Float> newValues) {
-        List<Configuration> configs = new ArrayList<>();
+    public static List<Configuration> getConfigurationsList(List<Configuration> configs, List<Float> newValues) {
 
-//        if(configs.size() != newValues.size()) {
-//            throw new RuntimeException("configuration and new values lists not in the same size");
-//        }
-        //TODO this function should be generalized and just take the values and automatically transfer them to list of configurations and do all the needed mapping
+        if(configs.size() != newValues.size()) {
+            throw new RuntimeException("configuration and new values lists not in the same size");
+        }
 
-//        for (int i = 0; i < configs.size(); i++) {
-//            if(configs.get(i).isBoolean()) {
-//                if(newValues.get(i) > 0.5) {
-//                    configs.get(i).setValue(1.0f);
-//                } else {
-//                    configs.get(i).setValue(0.0f);
-//                }
-//            } else if(configs.get(i).isInteger()) {
-//                configs.get(i).setValue(Math.round(newValues.get(i)));
-//            } else {
-//                configs.get(i).setValue(newValues.get(i));
-//            }
-//        }
-//
+        for (int i = 0; i < configs.size(); i++) {
+            if(configs.get(i).isBoolean()) {
+                if(newValues.get(i) > 0.5) {
+                    configs.get(i).setValue(1.0f);
+                } else {
+                    configs.get(i).setValue(0.0f);
+                }
+            } else if(configs.get(i).isInteger()) {
+                configs.get(i).setValue(Math.round(newValues.get(i)));
+            } else {
+                configs.get(i).setValue(newValues.get(i));
+            }
+        }
+
         return configs;
 
     }
 
+    public static List<Configuration> getConfigurationsList(List<Float> configurationsSample) {
+        return null;
+    }
 }
