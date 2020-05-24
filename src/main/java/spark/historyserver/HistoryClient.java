5d5420fd8b9fec9044f0b4472d9f6a83cd1bf44a
kamal <kamalhabieb@yahoo.ca>
2020-05-24 12:20:18 +0200

    [master] write environment, jobs, stages and executors json strings of last application into json files

--- src/main/java/spark/historyserver/HistoryClient.java
+++ src/main/java/spark/historyserver/HistoryClient.java
@@ -1,16 +1,13 @@
 package spark.historyserver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Comparator;
-import java.util.Date;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.http.HttpResponse;
@@ -24,12 +21,12 @@ import spark.historyserver.report.Report;
 import spark.historyserver.report.Summary;
 
 public class HistoryClient {
-    
-    private static final String APPLICATIONS_API            = "http://%s/api/v1/applications?status=completed&limit=%s&minDate=%s&maxDate=%s"; 
+
+    private static final String APPLICATIONS_API            = "http://%s/api/v1/applications?status=completed";
     private static final String APPLICATION_EXECUTORS_API   = "http://%s/api/v1/applications/%s/executors";
     private static final String APPLICATION_JOBS_API        = "http://%s/api/v1/applications/%s/jobs";
     private static final String APPLICATION_STAGES_API      = "http://%s/api/v1/applications/%s/stages";
-    private static final String APPLICATION_ENVIRONMENT_API        = "http://%s/api/v1/applications/%s/environment";
+    private static final String APPLICATION_ENVIRONMENT_API = "http://%s/api/v1/applications/%s/environment";
 
     private static InputStream request(DefaultHttpClient httpClient, String url) throws ClientProtocolException, IOException {
         HttpGet getRequest = new HttpGet(url);
@@ -40,7 +37,7 @@ public class HistoryClient {
         }
         return response.getEntity().getContent();
     }
-    
+
     private static String read(InputStream inputStream) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
         StringBuilder output = new StringBuilder();
@@ -50,7 +47,7 @@ public class HistoryClient {
         }
         return output.toString();
     }
-    
+
     private static <O, T> T aggregate(O[] arr, Function<O, T> e, BiFunction<T, T, T> acc) {
         T total = null;
         for(O o: arr) {
@@ -59,7 +56,7 @@ public class HistoryClient {
         }
         return total;
     }
-    
+
     private static void info(String message) {
         System.out.println(message);
     }
@@ -68,11 +65,42 @@ public class HistoryClient {
         System.err.println(message);
         exception.printStackTrace();
     }
-    
-    private static void process(String host, int limit, String filter, Date from, Date to, String outputPath) throws ClientProtocolException, IOException {
-        final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String minDate = formatter.format(from);
-        String maxDate = formatter.format(to);
+
+    private static String convertInputStreamToString(InputStream inputStream) {
+        return new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8)).lines()
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static void writeStream(InputStream inputStream, String targetFile) {
+        try {
+            String inputStreamString = convertInputStreamToString(inputStream);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(targetFile));
+            writer.write(inputStreamString);
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static InputStream clone(InputStream inputStream) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buffer)) > -1) {
+                byteArrayOutputStream.write(buffer, 0, len);
+            }
+            byteArrayOutputStream.flush();
+
+            return new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return inputStream;
+    }
+
+    private static void process(String host, int limit, String outputPath) throws ClientProtocolException, IOException {
         DefaultHttpClient httpClient = new DefaultHttpClient();
         try {
             ObjectMapper objectMapper = new ObjectMapper();
@@ -80,91 +108,50 @@ public class HistoryClient {
                 info(String.format("Connecting to %s without limit", host));
             else
                 info(String.format("Connecting to %s with limit %s", host, limit));
-            try (InputStream appInputStream = request(httpClient, String.format(APPLICATIONS_API, host, limit, minDate, maxDate))) {
+            try (InputStream appInputStream = request(httpClient, String.format(APPLICATIONS_API, host, limit))) {
                 Application[] history = objectMapper.readValue(read(appInputStream).getBytes(), Application[].class);
-                Stream<Application> applications = Arrays.stream(history);
-                if (filter == null) {
-                    info(String.format("Found %s applications between %s and %s", history.length, minDate, maxDate));
-                } else {
-                    long matched = applications.filter(application -> application.getName().contains(filter)).count();
-                    applications = Arrays.stream(history).filter(application -> application.getName().contains(filter));
-                    info(String.format("Found %s applications between %s and %s with filter '%s'", matched, minDate, maxDate, filter)); 
-                }
+                List<Application> applications = Collections.singletonList(history[0]);
                 Report report = new Report();
-                applications.map(application -> {
+                applications.stream().forEach(application -> {
                     try {
-                        Summary summary = new Summary();
-                        summary.setApplicationName(application.getName());
                         try (InputStream inputStream = request(httpClient, String.format(APPLICATION_ENVIRONMENT_API, host, application.getId()))) {
-                            Environment env = objectMapper.readValue(read(inputStream).getBytes(), Environment.class);
-                            Arrays.sort(env.getSparkProperties(), Comparator.comparing(p -> p[0]));
-                            for(String[] sparkProp: env.getSparkProperties()) {
-                                summary.addSparkProperty(sparkProp[0], sparkProp[1]);
-                            }
+                            String targetFilePath = "resources/"+application.getName()+"_environment.json";
+                            writeStream(inputStream, targetFilePath);
                         }
-                        summary.setAttemptsCount(application.getAttempts().length);
-                        summary.setDuration(application.getAttempts()[application.getAttempts().length - 1].getDuration());
                         try (InputStream inputStream = request(httpClient, String.format(APPLICATION_JOBS_API, host, application.getId()))) {
-                            Job[] jobs = objectMapper.readValue(read(inputStream).getBytes(), Job[].class);
-                            summary.setJobsCount(jobs.length);
-                            summary.setTasksPerJob(aggregate(jobs, j -> String.valueOf(j.getNumTasks()), (v1, v2) -> v1 + " + " + v2));
+                            String targetFilePath = "resources/"+application.getName()+"_jobs.json";
+                            writeStream(inputStream, targetFilePath);
                         }
                         try(InputStream inputStream = request(httpClient, String.format(APPLICATION_STAGES_API, host, application.getId()))) {
-                            Stage[] stages = objectMapper.readValue(read(inputStream).getBytes(), Stage[].class);
-                            summary.setStagesCount(stages.length);
-                            summary.setStagesInputBytes(aggregate(stages, s -> s.getInputBytes(), Math::addExact));
-                            summary.setStagesInputRecords(aggregate(stages, s -> s.getInputRecords(), Math::addExact));
-                            summary.setStagesOutputBytes(aggregate(stages, s -> s.getOutputBytes(), Math::addExact));
-                            summary.setStagesOutputRecords(aggregate(stages, s -> s.getOutputRecords(), Math::addExact));
-                            summary.setStagesShuffleReadBytes(aggregate(stages, s -> s.getShuffleReadBytes(), Math::addExact));
-                            summary.setStagesShuffleReadRecords(aggregate(stages, s -> s.getShuffleReadRecords(), Math::addExact));
-                            summary.setStagesShuffleWriteBytes(aggregate(stages, s -> s.getShuffleWriteBytes(), Math::addExact));
-                            summary.setStagesShuffleWriteRecords(aggregate(stages, s -> s.getShuffleWriteRecords(), Math::addExact));
-                            summary.setStagesMemorySpill(aggregate(stages, s -> s.getMemoryBytesSpilled(), Math::addExact));
-                            summary.setStagesDiskSpill(aggregate(stages, s -> s.getDiskBytesSpilled(), Math::addExact));
+                            String targetFilePath = "resources/"+application.getName()+"_stages.json";
+                            writeStream(inputStream, targetFilePath);
                         }
                         try (InputStream inputStream = request(httpClient, String.format(APPLICATION_EXECUTORS_API, host, application.getId()))) {
-                            Executor[] vms = objectMapper.readValue(read(inputStream).getBytes(), Executor[].class);
-                            Executor[] executors = Arrays.stream(vms).filter(e -> !e.getId().equals("driver")).toArray(Executor[]::new);
-                            summary.setExecutorsCount(executors.length);
-                            summary.setCoresPerExecutor(aggregate(executors, e -> e.getTotalCores(), Math::addExact) / executors.length);
-                            summary.setMaxMemoryPerExecutor(aggregate(executors, e -> e.getMaxMemory(), Math::max));
-                            summary.setExecutorsProcessingTime(aggregate(executors, e -> e.getTotalDuration(), Math::addExact));
-                            summary.setExecutorsGCTime(aggregate(executors, e -> e.getTotalGCTime(), Math::addExact));
-                            summary.setExecutorsInputBytes(aggregate(executors, e -> e.getTotalInputBytes(), Math::addExact));
-                            summary.setTasksCount(aggregate(executors, e -> e.getTotalTasks(), Math::addExact));
-                            summary.setExecutorsFailedTasks(aggregate(executors, e -> e.getFailedTasks(), Math::addExact));
-                            summary.setExecutorsShuffleReadBytes(aggregate(executors, e -> e.getTotalShuffleRead(), Math::addExact));
-                            summary.setExecutorsShuffleWriteBytes(aggregate(executors, e -> e.getTotalShuffleWrite(), Math::addExact));
+                            String targetFilePath = "resources/"+application.getName()+"_executors.json";
+                            writeStream(inputStream, targetFilePath);
                         }
-                        return summary;
                     } catch (Exception exception) {
                         error("Failed to parse " + application.getId(), exception);
-                        return null;
                     }
-                })
-                  .forEach(summary -> {
-                      info(summary.toString());
-                      report.addRow(summary);
-                  });
+                });
                 report.save(outputPath);
             }
         } finally {
             httpClient.getConnectionManager().shutdown();
         }
     }
-    
+
     public static void main(String[] args) {
         final String host = "localhost:18080";
         final int limit = Integer.MAX_VALUE;
-        final String filter = "AE_LINES_";
+        final String filter = null;
         Calendar calendar = Calendar.getInstance();
         Date to = calendar.getTime();
-        calendar.add(Calendar.DAY_OF_WEEK, -7);
+        calendar.add(Calendar.DAY_OF_WEEK, -100);
         Date from = calendar.getTime();
         String outputPath = String.format("history_%s_%s_%s.csv", filter, from.getTime(), to.getTime());
         try {
-            process(host, limit, filter, from, to, outputPath);
+            process(host, limit, outputPath);
         } catch (IOException e) {
             error("Failed to connect to History Server", e);
         }
--- src/test/java/SystemTrainerTest.java
+++ src/test/java/SystemTrainerTest.java
@@ -1,71 +1,71 @@
-import classification.Classifier;
-import classification.ClassifierImp;
-import configurations.ConfigurationsHandler;
-import configurations.ConfigurationsHandlerImp;
-import configurations.utilites.Configuration;
-import featuresextraction.utilities.Feature;
-import featuresextraction.FeaturesExtractionHandler;
-import featuresextraction.FeaturesExtractionHandlerImp;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.ArgumentMatchers;
-import spark.utilites.SparkApplication;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-public class SystemTrainerTest {
-    SystemTrainer systemTrainer;
-    FeaturesExtractionHandler featuresExtractionHandler;
-    ConfigurationsHandler configurationsHandler;
-    Classifier classifier;
-    SparkApplication sparkApplication;
-    List<Feature> features;
-    List<SparkApplication> startUpApplication;
-    Boolean success;
-
-    @Before
-    public void before() throws IOException {
-        sparkApplication = new SparkApplication();
-        startUpApplication = new ArrayList<SparkApplication>();
-        for (int i = 0; i < 5; i++) {
-            startUpApplication.add(new SparkApplication());
-        }
-        systemTrainer = mock(SystemTrainerImpl.class);
-        featuresExtractionHandler = mock(FeaturesExtractionHandlerImp.class);
-        configurationsHandler = mock(ConfigurationsHandlerImp.class);
-        classifier = mock(ClassifierImp.class);
-
-        when(featuresExtractionHandler.extract(any(SparkApplication.class))).thenReturn(new ArrayList<Feature>());
-        //when(configurationsHandler.applyApplication(any(SparkApplication.class))).thenReturn(new ArrayList<Configuration>());
-        when(classifier.trainModel(ArgumentMatchers.<List<Feature>>anyList() , ArgumentMatchers.<List<Configuration>>anyList())).thenReturn(true);
-
-        boolean simulationResult = simulateSystemTraining(startUpApplication);
-
-        when(systemTrainer.trainSystem(ArgumentMatchers.<SparkApplication>anyList())).thenReturn(simulationResult);
-
-    }
-
-
-    @Test
-    public void trainSystemTest() {
-        Assert.assertTrue(systemTrainer.trainSystem(startUpApplication));
-    }
-
-    private boolean simulateSystemTraining(List<SparkApplication> startUpApplication) throws IOException {
-        List<List<Feature>> features = new ArrayList<List<Feature>>();
-        List<List<Configuration>> configurations = new ArrayList<List<Configuration>>();
-        for (int i = 0; i < startUpApplication.size() ; i++) {
-            List<Feature> singleApplicationFeatures = featuresExtractionHandler.extract(startUpApplication.get(i));
-            features.add(singleApplicationFeatures);
-            //List<Configuration> singleApplicationConfiguration = configurationsHandler.applyApplication(startUpApplication.get(i));
-            //configurations.add(singleApplicationConfiguration);
-        }
-        return classifier.trainModel(features , configurations);
-    }
-}
+//import classification.Classifier;
+//import classification.ClassifierImp;
+//import configurations.ConfigurationsHandler;
+//import configurations.ConfigurationsHandlerImp;
+//import configurations.utilites.Configuration;
+//import featuresextraction.utilities.Feature;
+//import featuresextraction.FeaturesExtractionHandler;
+//import featuresextraction.FeaturesExtractionHandlerImp;
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.mockito.ArgumentMatchers;
+//import spark.utilites.SparkApplication;
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.when;
+//
+//public class SystemTrainerTest {
+//    SystemTrainer systemTrainer;
+//    FeaturesExtractionHandler featuresExtractionHandler;
+//    ConfigurationsHandler configurationsHandler;
+//    Classifier classifier;
+//    SparkApplication sparkApplication;
+//    List<Feature> features;
+//    List<SparkApplication> startUpApplication;
+//    Boolean success;
+//
+//    @Before
+//    public void before() throws IOException {
+//        sparkApplication = new SparkApplication();
+//        startUpApplication = new ArrayList<SparkApplication>();
+//        for (int i = 0; i < 5; i++) {
+//            startUpApplication.add(new SparkApplication());
+//        }
+//        systemTrainer = mock(SystemTrainerImpl.class);
+//        featuresExtractionHandler = mock(FeaturesExtractionHandlerImp.class);
+//        configurationsHandler = mock(ConfigurationsHandlerImp.class);
+//        classifier = mock(ClassifierImp.class);
+//
+//        when(featuresExtractionHandler.extract(any(SparkApplication.class))).thenReturn(new ArrayList<Feature>());
+//        //when(configurationsHandler.applyApplication(any(SparkApplication.class))).thenReturn(new ArrayList<Configuration>());
+//        when(classifier.trainModel(ArgumentMatchers.<List<Feature>>anyList() , ArgumentMatchers.<List<Configuration>>anyList())).thenReturn(true);
+//
+//        boolean simulationResult = simulateSystemTraining(startUpApplication);
+//
+//        when(systemTrainer.trainSystem(ArgumentMatchers.<SparkApplication>anyList())).thenReturn(simulationResult);
+//
+//    }
+//
+//
+//    @Test
+//    public void trainSystemTest() {
+//        Assert.assertTrue(systemTrainer.trainSystem(startUpApplication));
+//    }
+//
+//    private boolean simulateSystemTraining(List<SparkApplication> startUpApplication) throws IOException {
+//        List<List<Feature>> features = new ArrayList<List<Feature>>();
+//        List<List<Configuration>> configurations = new ArrayList<List<Configuration>>();
+//        for (int i = 0; i < startUpApplication.size() ; i++) {
+//            List<Feature> singleApplicationFeatures = featuresExtractionHandler.extract(startUpApplication.get(i));
+//            features.add(singleApplicationFeatures);
+//            //List<Configuration> singleApplicationConfiguration = configurationsHandler.applyApplication(startUpApplication.get(i));
+//            //configurations.add(singleApplicationConfiguration);
+//        }
+//        return classifier.trainModel(features , configurations);
+//    }
+//}
